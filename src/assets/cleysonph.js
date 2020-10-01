2647a44fad925a7e045eb2f8eb14f3cbc808b1ca
Cleyson Lima <cleysonph@gmail.com>
2020-10-01 15:10:41 -0300

    added myself to roll call

---
+++ src/assets/cleysonph.js
@@ -0,0 +1,7 @@
+export const cleysonph = {
+  "name": "Cleyson Lima",
+  "gif": "https://media.giphy.com/media/13FrpeVH09Zrb2/giphy.gif",
+  "skills": "JavaScript, Python, Git",
+  "github": "https://github.com/cleysonph"
+}
+export default cleysonph
