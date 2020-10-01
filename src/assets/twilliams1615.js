41e5159978d9e3644d3ad39b561112889ba07828
Cary Williams <cary@linux.com>
2020-10-01 12:59:41 -0500

    added myself to rollcall
    added myself to rollcall

---
+++ src/assets/twilliams1615.js
@@ -0,0 +1,7 @@
+        export const twilliams1615 = {
+            "name": "Cary Williams",
+            "gif": "http://gph.is/1aipsZY", // using gif link on https://giphy.com/
+            "skills": "bash, python, ruby",
+            "github": "https://github.com/twilliams1615"
+        }
+        export default twilliams1615
