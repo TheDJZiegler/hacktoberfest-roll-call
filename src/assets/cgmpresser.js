432ad9c6a71d6a28f8852e981f4bae78bad49ab1
cgmpresser <57267436+cgmpresser@users.noreply.github.com>
2020-10-01 14:47:37 -0400

    Clif is present

---
+++ src/assets/cgmpresser.js
@@ -0,0 +1,9 @@
+export const cgmpresser = {
+    "name": "Clif",
+    "gif": "https://media.giphy.com/media/3oEjHLzm4BCF8zfPy0/giphy.gif", // using gif link on https://gip
+hy.com/
+    "skills": "Java, JavaScript, C++, SQL (skills I've forgotten would be a much longer list)",
+    "github": "https://github.com/cgmpresser"
+}
+
+export default cgmpresser
