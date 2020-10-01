4cd4b7b32ca945a3d1e6323406fdde41c0aa955a
mendelevium28 <muskandeepkaur28@gmail.com>
2020-10-02 01:18:29 +0530

    added myself to roll call

---
+++ src/assets/mendelevium28.js
@@ -0,0 +1,7 @@
+export const mendelevium28 = {
+    "name": "Muskan deep Kaur",
+    "gif": "https://giphy.com/gifs/happy-pokemon-adorable-13G7hmmFr9yuxG", // using the gif link itself
+    "skills": "C++, Javascript, Html, CSS",
+    "github": "https://github.com/mendelevium28"
+}
+export default mendelevium28
