efae425e0998a4bc03fe474a6fafc36ffb55f67b
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2020-10-02 23:54:29 +0530

    added myself to roll call

---
+++ src/assets/nischaynagar.js
@@ -0,0 +1,7 @@
+export const nischaynagar = {
+    "name": "Nischay Nagar",
+    "gif": "https://media.giphy.com/media/duzpaTbCUy9Vu/giphy.gif", // using gif link on https://giphy.com/
+    "skills": "JavaScript, C, C++, Python",
+    "github": "https://github.com/nischaynagar"
+}
+export default nischaynagar
