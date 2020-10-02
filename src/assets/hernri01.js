66a6617a80aa2ac18ab7d480b49982d97eb95821
Danny <thedjziegler@gmail.com>
2020-10-02 17:15:11 -0400

    bug fixes

--- src/assets/hernri01.js
+++ src/assets/hernri01.js
@@ -1,6 +1,6 @@
   export const hernri01 = {
             "name": "Ricardo Hernandez",
-            "gif": "https://media.giphy.com/media/SzOLAe5Q7Jrby/giphy.gif"
+            "gif": "https://media.giphy.com/media/SzOLAe5Q7Jrby/giphy.gif",
             "skills": "I know Java, Python, PHP, SQL, HTML, but I want to learn how to use Github more",
             "github": "https://github.com/hernri01"
         }
