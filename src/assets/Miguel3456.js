67f3e697e19c878a7ec959663e86f6da8fdce56e
Miguel3456 <72050716+Miguel3456@users.noreply.github.com>
2020-10-02 17:01:17 -0500

    add Miguel to roll call

--- src/assets/Miguel3456.js
+++ src/assets/Miguel3456.js
@@ -1,6 +1,6 @@
 export const Miguel3456 = {
     "name": "Miguel",
-    "gif": "https://knowyourmeme.com/memes/woman-yelling-at-a-cat", 
+    "gif": "https://media2.giphy.com/media/JRE3AvLsSRXg360F6l/giphy.gif", 
     "skills": "Java(I'm still learning it), Javascript, and I would like to know Python",
     "github": "https://github.com/Miguel3456"
 }
--- src/assets/username.js.js
+++
@@ -1,8 +0,0 @@
-export const thedjziegler = {
-    "name": "Miguel",
-    "gif": "https://knowyourmeme.com/memes/woman-yelling-at-a-cat", 
-    "skills": "Java(I'm still learning it), Javascript, and I would like to know Python",
-    "github": "https://github.com/Miguel3456"
-}
-export default Miguel3456
-
