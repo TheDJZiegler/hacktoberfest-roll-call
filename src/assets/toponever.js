5db0967eb52f4bc8b68683bc2099ee7e7f8af68c
toponever <nile21za@hotmail.com>
2020-10-03 00:49:09 +0700

    cattoberFest

---
+++ src/assets/toponever.js
@@ -0,0 +1,7 @@
+export const toponever = {
+    "name": "Toponever",
+    "gif": "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif", // using the gif link itself
+    "skills": "Javascript,html,css",
+    "github": "https://github.com/toponever"
+}
+export default toponever
