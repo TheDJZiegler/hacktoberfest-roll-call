3ed792371de01b551c819a4f3df445d0dff39e3c
jtrancoso <jesus.trancoso.jimenez@gmai.com>
2020-10-02 13:51:28 +0200

    first Hacktoberfest, don't upvote

---
+++ src/jtrancoso.js
@@ -0,0 +1,7 @@
+export const jtrancoso = {
+            "name": "Jesús Trancoso Jiménez",
+            "gif": "https://media.giphy.com/media/kz06NMJSTjR9UotwO1/giphy.gif", // using gif link on https://giphy.com/
+            "skills": "C",
+            "github": "https://github.com/jtrancoso"
+        }
+        export default jtrancoso
