c3392e686d61b56a9120fec8d8edaa2ff02286c4
Ricardo Hernandez <40433585+hernri01@users.noreply.github.com>
2020-10-02 16:01:39 -0400

    Create hernri01.js

---
+++ src/assets/hernri01.js
@@ -0,0 +1,7 @@
+  export const hernri01 = {
+            "name": "First Last",
+            "gif": "url of you favorite gif", // using gif link on https://giphy.com/
+            "skills": "languages you know or want to learn more of during Hacktoberfest",
+            "github": "https://github.com/USERNAME"
+        }
+        export default hernri01
