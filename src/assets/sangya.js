56e21d99d8641b7c06f6860de2946c6f9086935d
Sangya Lohani <lohanisangya@gmail.com>
2020-10-02 16:13:56 -0400

    added myslef to roll call

---
+++ src/assets/sangya.js
@@ -0,0 +1,7 @@
+export const sangya = {
+    "name": "Sangya Lohani",
+    "gif": "https://media.giphy.com/media/lp3GUtG2waC88/giphy.gif", // using the gif link itself
+    "skills": "Java",
+    "github": "https://github.com/lohanisangya"
+}
+export default sangya
