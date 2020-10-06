fe6139a8a57f8cbf7cd8b975f8c55309c49c4d8a
Sandeep Gamot <sandygamot77@gmail.com>
2020-10-06 12:23:28 +0530

    stabcode.js with dancing DJ Khaled:joy:

---
+++ src/assets/stabcode.js
@@ -0,0 +1,7 @@
+export const stabcode = {
+    "name": "Sandeep Gamot",
+    "gif": "https://media.giphy.com/media/l4FGpPki5v2Bcd6Ss/giphy.gif", // after making pr to this repo😂
+    "skills": "React!❤️, Node, Express, Java",
+    "github": "https://github.com/SandeepGamot"
+}
+export default stabcode
