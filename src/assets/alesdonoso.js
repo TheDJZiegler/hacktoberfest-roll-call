3f2e89cb1eb5d1339b76cd571a071b34eeea507a
Alejandro Donoso <aledonoso@MacBook-Pro-de-Alejandro.local>
2020-10-27 09:57:04 +0100

    added myself to roll call

---
+++ src/assets/alesdonoso.js
@@ -0,0 +1,7 @@
+export const alesdonoso = {
+    "name": "alesdonoso",
+    "gif": "https://media.giphy.com/media/9mZoOe2CWoeha/giphy.gif", // using the gif link itself
+    "skills": "JavaScript, CSS",
+    "github": "https://github.com/alesdonoso"
+}
+export default alesdonoso
