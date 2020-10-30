607fff00edcf5ae4c891ea303ab2918e2018fb73
vijaykrishna <vijay.krishna@gdn-commerce.com>
2020-10-31 00:49:04 +0530

    Added myself to roll call

---
+++ src/assets/vijaykrishna.js
@@ -0,0 +1,7 @@
+export const vijaykrishna = {
+	"name": "Vijay Krishna",
+	"gif": "https://media3.giphy.com/media/kxMQXnH7ucS9q/giphy.gif",
+	"skills": "JS, Java, CSS, Python, C",
+	"github": "https://github.com/vijaykrishna536"
+}
+export default vijaykrishna 
